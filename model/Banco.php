91d4fae4045bba95590cd9048309c18b88a4cb8e
alanfacco <alanfacco@gmail.com>
2013-11-06 14:29:53 -0200

    Banco

--- model/Banco.php
+++ model/Banco.php
@@ -5,10 +5,7 @@ class Banco {
     private $dsn = "mysql:dbname=salgaderia;host=127.0.0.1";
     private $usuario = "salgaderia";
     private $senha = "123456";
-    
-//    private $dsn = "mysql:dbname=u789707197_salga;host=mysql.hostinger.com.br";
-//    private $usuario = "u789707197_salga";
-//    private $senha = "dnpm8450";
+
     public $conn = NULL;
 
     public function __construct() {
